7dac4a56404dbff2bde7240f1ebc8d2b4d8f51f3
Arpit Makkar <168638918+ArpitMakkar12@users.noreply.github.com>
2026-08-09 18:32:26 +0530

    Time: 815 ms (19.88%), Space: 0B (100%) - LeetHub

--- 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -5,5 +5,5 @@ Select e2.employee_id, e2.name,
 From Employees e1 
 Join Employees e2
 on e1.reports_to = e2.employee_id
-group by e2.employee_id
+group by e2.employee_id, e2.name
 order by e2.employee_id;
